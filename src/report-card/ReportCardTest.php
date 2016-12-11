2545401364bec7afc19e7f74b04e1c073cda8d15
Andrea Stronati <astronati@vendini.com>
2016-12-11 01:17:11 +0100

    ticket #47 Replaced arrays with short definition

--- src/report-card/ReportCardTest.php
+++ src/report-card/ReportCardTest.php
@@ -9,117 +9,117 @@ class ReportCardTest extends PHPUnit_Framework_TestCase {
 
     public function dataProvider() {
         return array(
-            array(
+            [
                 // First Strings Quotations
-                array(
-                    array('id' => 1, 'vote' => 5, 'magicPoints' => 6),
-                    array('id' => 2, 'vote' => 6, 'magicPoints' => 7),
-                    array('id' => 3, 'vote' => 7, 'magicPoints' => 8),
-                    array('id' => 4, 'vote' => 8, 'magicPoints' => 9),
-                ),
+                [
+                    ['id' => 1, 'vote' => 5, 'magicPoints' => 6],
+                    ['id' => 2, 'vote' => 6, 'magicPoints' => 7],
+                    ['id' => 3, 'vote' => 7, 'magicPoints' => 8],
+                    ['id' => 4, 'vote' => 8, 'magicPoints' => 9],
+                ],
                 // Reserves Quotations
-                array(
-                    array('id' => 5, 'vote' => 4, 'magicPoints' => 5),
-                    array('id' => 6, 'vote' => 3, 'magicPoints' => 4),
-                ),
+                [
+                    ['id' => 5, 'vote' => 4, 'magicPoints' => 5],
+                    ['id' => 6, 'vote' => 3, 'magicPoints' => 4],
+                ],
                 // Use magic points
                 true,
                 // Expected result
-                array(6,7,8,9)
-            ),
-            array(
-                array(
-                    array('id' => 1, 'vote' => 5, 'magicPoints' => 6),
-                    array('id' => 2, 'vote' => null, 'magicPoints' => 7),
-                    array('id' => 3, 'vote' => 7, 'magicPoints' => 8),
-                    array('id' => 4, 'vote' => 8, 'magicPoints' => 9),
-                ),
-                array(
-                    array('id' => 5, 'vote' => null, 'magicPoints' => 5),
-                    array('id' => 6, 'vote' => 3, 'magicPoints' => 4),
-                ),
+                [6,7,8,9],
+            ],
+            [
+                [
+                    ['id' => 1, 'vote' => 5, 'magicPoints' => 6],
+                    ['id' => 2, 'vote' => null, 'magicPoints' => 7],
+                    ['id' => 3, 'vote' => 7, 'magicPoints' => 8],
+                    ['id' => 4, 'vote' => 8, 'magicPoints' => 9],
+                ],
+                [
+                    ['id' => 5, 'vote' => null, 'magicPoints' => 5],
+                    ['id' => 6, 'vote' => 3, 'magicPoints' => 4],
+                ],
                 true,
-                array(6,7,8,9)
-            ),
-            array(
-                array(
-                    array('id' => 1, 'vote' => 5, 'magicPoints' => 6),
-                    array('id' => 2, 'vote' => 6, 'magicPoints' => 7),
-                    array('id' => 3, 'vote' => 7, 'magicPoints' => 8),
-                    array('id' => 4, 'vote' => 8, 'magicPoints' => 9),
-                ),
-                array(
-                    array('id' => 5, 'vote' => 4, 'magicPoints' => 5),
-                    array('id' => 6, 'vote' => 3, 'magicPoints' => 4),
-                ),
+                [6,7,8,9],
+            ],
+            [
+                [
+                    ['id' => 1, 'vote' => 5, 'magicPoints' => 6],
+                    ['id' => 2, 'vote' => 6, 'magicPoints' => 7],
+                    ['id' => 3, 'vote' => 7, 'magicPoints' => 8],
+                    ['id' => 4, 'vote' => 8, 'magicPoints' => 9],
+                ],
+                [
+                    ['id' => 5, 'vote' => 4, 'magicPoints' => 5],
+                    ['id' => 6, 'vote' => 3, 'magicPoints' => 4],
+                ],
                 false,
-                array(5,6,7,8)
-            ),
-            array(
-                array(
-                    array('id' => 1, 'vote' => 5, 'magicPoints' => 6),
-                    array('id' => 2, 'vote' => 6, 'magicPoints' => 7),
-                    array('id' => 3, 'vote' => 7, 'magicPoints' => 8)
-                ),
-                array(
-                    array('id' => 4, 'vote' => 4, 'magicPoints' => 5),
-                    array('id' => 5, 'vote' => 3, 'magicPoints' => 4),
-                ),
+                [5,6,7,8],
+            ],
+            [
+                [
+                    ['id' => 1, 'vote' => 5, 'magicPoints' => 6],
+                    ['id' => 2, 'vote' => 6, 'magicPoints' => 7],
+                    ['id' => 3, 'vote' => 7, 'magicPoints' => 8],
+                ],
+                [
+                    ['id' => 4, 'vote' => 4, 'magicPoints' => 5],
+                    ['id' => 5, 'vote' => 3, 'magicPoints' => 4],
+                ],
                 false,
-                array(5,6,7)
-            ),
-            array(
-                array(
-                    array('id' => 1, 'vote' => 5, 'magicPoints' => 6),
-                    array('id' => 2, 'vote' => 6, 'magicPoints' => 7),
-                    array('id' => 3, 'vote' => 7, 'magicPoints' => 8)
-                ),
-                array(
-                    array('id' => 4, 'vote' => 4, 'magicPoints' => 5),
-                    array('id' => 5, 'vote' => 3, 'magicPoints' => 4),
-                ),
+                [5,6,7],
+            ],
+            [
+                [
+                    ['id' => 1, 'vote' => 5, 'magicPoints' => 6],
+                    ['id' => 2, 'vote' => 6, 'magicPoints' => 7],
+                    ['id' => 3, 'vote' => 7, 'magicPoints' => 8],
+                ],
+                [
+                    ['id' => 4, 'vote' => 4, 'magicPoints' => 5],
+                    ['id' => 5, 'vote' => 3, 'magicPoints' => 4],
+                ],
                 true,
-                array(6,7,8)
-            ),
-            array(
-                array(
-                    array('id' => 1, 'vote' => null, 'magicPoints' => null),
-                    array('id' => 2, 'vote' => 6, 'magicPoints' => 6.5),
-                    array('id' => 3, 'vote' => 7, 'magicPoints' => 7)
-                ),
-                array(
-                    array('id' => 4, 'vote' => 4, 'magicPoints' => 5),
-                    array('id' => 5, 'vote' => 3, 'magicPoints' => 4),
-                ),
+                [6,7,8],
+            ],
+            [
+                [
+                    ['id' => 1, 'vote' => null, 'magicPoints' => null],
+                    ['id' => 2, 'vote' => 6, 'magicPoints' => 6.5],
+                    ['id' => 3, 'vote' => 7, 'magicPoints' => 7],
+                ],
+                [
+                    ['id' => 4, 'vote' => 4, 'magicPoints' => 5],
+                    ['id' => 5, 'vote' => 3, 'magicPoints' => 4],
+                ],
                 true,
-                array(5,6.5,7)
-            ),
-            array(
-                array(
-                    array('id' => 1, 'vote' => 5, 'magicPoints' => 0),
-                    array('id' => 2, 'vote' => 6, 'magicPoints' => 6.5),
-                    array('id' => 3, 'vote' => 7, 'magicPoints' => 7)
-                ),
-                array(
-                    array('id' => 4, 'vote' => 4, 'magicPoints' => 6),
-                    array('id' => 5, 'vote' => 3, 'magicPoints' => 4),
-                ),
+                [5,6.5,7],
+            ],
+            [
+                [
+                    ['id' => 1, 'vote' => 5, 'magicPoints' => 0],
+                    ['id' => 2, 'vote' => 6, 'magicPoints' => 6.5],
+                    ['id' => 3, 'vote' => 7, 'magicPoints' => 7],
+                ],
+                [
+                    ['id' => 4, 'vote' => 4, 'magicPoints' => 6],
+                    ['id' => 5, 'vote' => 3, 'magicPoints' => 4],
+                ],
                 true,
-                array(0,6.5,7)
-            ),
-            array(
-                array(
-                    array('id' => 1, 'vote' => 5, 'magicPoints' => 0),
-                    array('id' => 2, 'vote' => 6, 'magicPoints' => 6.5),
-                    array('id' => 3, 'vote' => 7, 'magicPoints' => 7)
-                ),
-                array(
-                    array('id' => 4, 'vote' => 4, 'magicPoints' => 6),
-                    array('id' => 5, 'vote' => 3, 'magicPoints' => 4),
-                ),
+                [0,6.5,7],
+            ],
+            [
+                [
+                    ['id' => 1, 'vote' => 5, 'magicPoints' => 0],
+                    ['id' => 2, 'vote' => 6, 'magicPoints' => 6.5],
+                    ['id' => 3, 'vote' => 7, 'magicPoints' => 7],
+                ],
+                [
+                    ['id' => 4, 'vote' => 4, 'magicPoints' => 6],
+                    ['id' => 5, 'vote' => 3, 'magicPoints' => 4],
+                ],
                 false,
-                array(5,6,7)
-            ),
+                [5,6,7],
+            ],
         );
     }
 
